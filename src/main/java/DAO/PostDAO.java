b05d7044794435d9c4f9822d55143b2dc06f8b0e
Kutou01 <53705900+Kutou01@users.noreply.github.com>
2024-10-24 11:43:25 +0700

    Update PostDAO.java

--- src/main/java/DAO/PostDAO.java
+++ src/main/java/DAO/PostDAO.java
@@ -1,6 +1,9 @@
 package DAO;
 
 import java.util.List;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -8,6 +11,7 @@ import org.hibernate.Transaction;
 
 import HibernateUtil.HibernateUtil;
 import Model.Post;
+import Model.User;
 
 public class PostDAO implements InterfaceDAO<Post> {
 	@Override
@@ -67,6 +71,20 @@ public class PostDAO implements InterfaceDAO<Post> {
 		return result;
 	}
 
+	public List<Post> getPostsByUser(User user) {
+		List<Post> posts = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			Transaction transaction = session.beginTransaction();
+			posts = session.createQuery("from Post where user = :user order by createdAt desc", Post.class)
+					.setParameter("user", user)
+					.list();
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return posts;
+	}
+
 	@Override
 	public Post selectById(Post t) {
 		// TODO Auto-generated method stub
@@ -79,4 +97,35 @@ public class PostDAO implements InterfaceDAO<Post> {
 		return null;
 	}
 
+	public void exportPostsToSQL(String filePath) {
+		List<Post> posts = selectAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		try (PrintWriter writer = new PrintWriter(new FileWriter(filePath, true))) {
+			writer.println("LOCK TABLES `post` WRITE;");
+			writer.println("/*!40000 ALTER TABLE `post` DISABLE KEYS */;");
+			writer.println("INSERT INTO `post` VALUES");
+
+			for (int i = 0; i < posts.size(); i++) {
+				Post post = posts.get(i);
+				writer.printf("('%s','%s','%s','%s','%s')",
+					post.getPostId(),
+					post.getPostContent().replace("'", "\\'"),
+					post.getPostImage(),
+					sdf.format(post.getCreatedAt()),
+					post.getUser().getUserId());
+
+				if (i < posts.size() - 1) {
+					writer.println(",");
+				} else {
+					writer.println(";");
+				}
+			}
+
+			writer.println("/*!40000 ALTER TABLE `post` ENABLE KEYS */;");
+			writer.println("UNLOCK TABLES;");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
